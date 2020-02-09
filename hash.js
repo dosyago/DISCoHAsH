c48505e486e06b97d36c8e1628f38a83a70fff5b
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-02-09 19:05:11 +0000

    "JS imp is WIP"

--- hash.js
+++ hash.js
@@ -3,7 +3,7 @@ export const BEBB4185 = discohash;
 const STATE = 32;
 const STATE64 = STATE >> 3;
 const STATEM = STATE-1;
-const BSTATEM = BigInt(STATEM);
+const BSTATEM = (STATEM);
 const HSTATE64M = (STATE64 >> 1)-1;
 const STATE64M = STATE64-1;
 const disco_buf = new ArrayBuffer(STATE);
@@ -12,6 +12,10 @@ const Q = 13166748625691186689n;
 const ds8 = new Uint8Array(disco_buf);
 const ds32 = new Uint32Array(disco_buf);
 const ds = new BigUint64Array(disco_buf);
+const C = new BigUint64Array(1);
+const C8 = new Uint8Array(1);
+const T64 = new BigUint64Array(1);
+const T8 = new Uint8Array(1);
 const R = 23n;
 const S3 = 63n;
 const S4 = 64n;
@@ -34,6 +38,7 @@ function rot8( v, n = 0 ) {
 
 function mix(A = 0) {
 	const B = A+1;
+  
 	ds[A] *= P;
 	ds[A] = rot(ds[A], R);
 	ds[A] *= Q;
@@ -47,14 +52,15 @@ function mix(A = 0) {
 
 function round( m64, m8 ) {
 	const len = m8.length;
-	let index = 0n;
-	let counter = 0xfaccadaccad09997n;
+	let index = 0;
 	let sindex = 0;
-	let counter8 = 137;
+	C[0] = 0xfaccadaccad09997n;
+  C8[0] = 137;
 
 	for( let Len = len >> 3; index < Len; index++) {
-		ds[sindex] += rot(m64[index] + index + counter + 1n, R);
-		counter += ~m64[index] + 1n;
+		T64[0] = m64[index] + BigInt(index) + C[0] + 1n;
+    ds[sindex] += rot(T64[0], R);
+		C[0] += ~m64[index] + 1n;
 		if ( sindex == HSTATE64M ) {
 			mix(0);
 		} else if ( sindex == STATE64M ) {
@@ -66,11 +72,12 @@ function round( m64, m8 ) {
 
 	mix(1);
 
-	index <<= 3n;
+	index <<= 3;
 	sindex = index&(BSTATEM);
 	for( ; index < len; index++) {
-		ds8[sindex] += rot8(m8[index] + index + counter8 + 1, 23);
-		counter8 += ~m8[sindex] + 1;
+		T8[0] = m8[index] + index + C8[0] + 1;
+    ds8[sindex] = rot8(T8[0], 23);
+		C8[0] += ~m8[sindex] + 1;
 		mix(index%STATE64M);
 		if ( sindex >= STATEM ) {
 			sindex = -1;
@@ -94,7 +101,11 @@ export function discohash( key = '', seed = 0 ) {
     key = Key;
   }
 
-  const len = key.length*4;
+  let len = key.length*4;
+  if( len % 8 != 0 ) {
+    len += 4;
+  }
+
   const key_buf = new ArrayBuffer(len);
   const key8Arr = new Uint8Array(key_buf);
   const key32Arr = new Uint32Array(key_buf);
