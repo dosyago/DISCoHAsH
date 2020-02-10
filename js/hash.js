1bc43078a1aa23ba6c381830597e78e9f452d044
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-02-10 09:21:53 +0000

    Exploring JS IMP WIP

---
+++ js/hash.js
@@ -0,0 +1,167 @@
+export const BEBB4185 = discohash;
+
+const STATE = 32;
+const STATE64 = STATE >> 3;
+const STATEM = STATE-1;
+const BSTATEM = (STATEM);
+const HSTATE64M = (STATE64 >> 1)-1;
+const STATE64M = STATE64-1;
+const disco_buf = new ArrayBuffer(STATE);
+const P = 0xFFFFFFFFFFFFFFFFn - 58n;
+const Q = 13166748625691186689n;
+const ds8 = new Uint8Array(disco_buf);
+const ds32 = new Uint32Array(disco_buf);
+const ds = new BigUint64Array(disco_buf);
+const C = new BigUint64Array(1);
+const C8 = new Uint8Array(1);
+const T64 = new BigUint64Array(1);
+const T8 = new Uint8Array(1);
+const R = 23n;
+const S3 = 63n;
+const S4 = 64n;
+
+function rot( v, n = 0n ) {
+	n = n & S3;
+	if (n) {
+		v = (v >> n) | (v << (S4-n));
+	}
+	return v; 
+}
+
+function rot8( v, n = 0 ) {
+	n = n & 7;
+	if (n) {
+		v = (v >> n) | (v << (8-n));
+	}
+	return v; 
+}
+
+function mix(A = 0) {
+	const B = A+1;
+  
+	ds[A] *= P;
+	ds[A] = rot(ds[A], R);
+	ds[A] *= Q;
+	
+	ds[B] ^= ds[A];
+
+	ds[B] *= P;
+	ds[B] = rot(ds[B], R);
+	ds[B] *= Q;
+}
+
+function round( m64, m8, len ) {
+	let index = 0;
+	let sindex = 0;
+	C[0] = 0xfaccadaccad09997n;
+  C8[0] = 137;
+
+	for( let Len = len >> 3; index < Len; index++) {
+		T64[0] = m64[index] + BigInt(index) + C[0] + 1n;
+    T64[0] = rot(T64[0], R);
+    //console.log(`rds1 = 0x${ds[0].toString(16).padStart(16,'0')} 0x${ds[1].toString(16).padStart(16,'0')} 0x${ds[2].toString(16).padStart(16,'0')} 0x${ds[3].toString(16).padStart(16,'0')}`);
+    ds[sindex] += T64[0];
+		T64[0] = ~m64[index] + 1n;
+		C[0] += T64[0];
+		if ( sindex == HSTATE64M ) {
+			mix(0);
+		} else if ( sindex == STATE64M ) {
+			mix(2);
+			sindex = -1;
+		}
+		sindex++;
+    //console.log(`rds2 = 0x${ds[0].toString(16).padStart(16,'0')} 0x${ds[1].toString(16).padStart(16,'0')} 0x${ds[2].toString(16).padStart(16,'0')} 0x${ds[3].toString(16).padStart(16,'0')}`);
+	}
+
+	mix(1);
+
+	index <<= 3;
+	sindex = index&(BSTATEM);
+	for( ; index < len; index++) {
+		T8[0] = m8[index] + index + C8[0] + 1;
+    ds8[sindex] += rot8(T8[0], 23);
+		T8[0] = ~m8[sindex] + 1;
+    C8[0] += T8[0];
+		mix(index%STATE64M);
+		if ( sindex >= BSTATEM ) {
+			sindex = -1;
+		}
+		sindex++;
+	}
+
+	mix(0);
+	mix(1);
+	mix(2);
+}
+
+export function discohash( key = '', seed = 0 ) {
+  // get 32-bit codepoints if it's a string
+    // ref: "I say we take take-off, nuke the site from orbit, only way to be sure"
+
+  let wasString = false;
+  let len = key.length;
+  if ( typeof key == "string" ) {
+    const Key = [];
+    for( const k of key ) {
+      Key.push(k.codePointAt(0));
+    }
+    key = Key;
+    len = key.length*4;
+    if( len % 8 != 0 ) {
+      len += 4;
+    }
+    wasString = true;
+  }
+
+  const blen = len + (8-(len%8))%8;
+  //console.log(JSON.stringify(Array.from(key)), seed, len, blen);
+  const key_buf = new ArrayBuffer(blen);
+  const key8Arr = new Uint8Array(key_buf);
+  const key32Arr = new Uint32Array(key_buf);
+  const key64Arr = new BigUint64Array(key_buf);
+
+  if ( wasString ) {
+    key32Arr.set(key);
+  } else {
+    key8Arr.set(key);
+  }
+
+  const seed_buf = new ArrayBuffer(16);
+  const seed8Arr = new Uint8Array(seed_buf);
+  const seed32Arr = new Uint32Array(seed_buf);
+  const seed64Arr = new BigUint64Array(seed_buf);
+
+  seed32Arr[0] = 0xc5550690;
+  seed32Arr[0] -= seed;
+  seed32Arr[1] = 1 + seed;
+  seed32Arr[2] = 1 - seed;
+  seed32Arr[2] = ~seed32Arr[2];
+  seed32Arr[3] = 1 + seed;
+  seed32Arr[3] *= 0xf00dacca;
+
+  // nothing up my sleeve
+  ds[0] = 0x123456789abcdef0n;
+  ds[1] = 0x0fedcba987654321n;
+  ds[2] = 0xaccadacca80081e5n;
+  ds[3] = 0xf00baaf00f00baaan;
+
+  //console.log(`ds = 0x${ds[0].toString(16).padStart(16,'0')} 0x${ds[1].toString(16).padStart(16,'0')} 0x${ds[2].toString(16).padStart(16,'0')} 0x${ds[3].toString(16).padStart(16,'0')}`);
+
+  round( key64Arr, key8Arr, len );
+  round( seed64Arr, seed8Arr, 16 );
+  round( ds, ds8, STATE );
+
+  /**
+  console.log(`ds = ${ds[0]} ${ds[1]} ${ds[2]} ${ds[3]}\n`);
+  **/
+
+  const output = new ArrayBuffer(STATE);
+  const h = new BigUint64Array(output);
+
+  h[0] = ds[2];
+  h[1] = ds[3];
+
+  h[0] += h[1];
+
+  return h[0];
+}
---
+++ js/test.js
@@ -0,0 +1,69 @@
+import {discohash} from './index.js';
+
+const tests = [
+  [ 'abc123', 0 ],
+  [ 'xyz999', 2 ]
+];
+
+test();
+smhasher_verification_value();
+
+function test() {
+  tests.forEach(([k, s]) => {
+    console.log(`${k}, ${s}, 0x${discohash(k, s).toString(16).padStart(16,'0')}`);
+  });
+}
+
+function swap(a,i,b,j) {
+  a[i] ^= b[j];
+  b[j] ^= a[i];
+  a[i] ^= b[j];
+}
+
+function swapb(a,i) {
+  a[i] = (a[i] << 4) | (a[i] >> 4);
+}
+
+function smhasher_verification_value() {
+	// Copied from <smhasher_repo>/src/KeysetTests.cpp
+	
+	const hashbytes = 8;
+	const key = new Uint8Array( 256 );
+  const hbuf = new ArrayBuffer(256*hashbytes);
+	const hashes = new Uint8Array(hbuf);
+  const out = new ArrayBuffer(hashbytes);
+  const hash = new Uint8Array(out);
+  const h32 = new Uint32Array(out);
+  const hout = new BigUint64Array(out);
+  const verif = new Uint32Array(1);
+
+	// Hash keys of the form {0}, {0,1}, {0,1,2}... up to N=255,using 256-N as
+	// the seed
+	for ( let i = 0; i < 256; i++ ) {
+		key[i] = i;
+
+		const seed = 256-i;
+
+		hout[0] = discohash(key.slice(0,i+1), seed );
+
+		hashes.set( hash, i*hashbytes );
+	}
+
+	// Then hash the result array
+
+	hout[0] = discohash(hashes, 0);
+  // equivalentj
+  // verif[0] = hash[0] | (hash[1] << 8) | (hash[2] << 16) | (hash[3] << 24);
+  /**
+  swap(hash,0,hash,2);
+  swap(hash,1,hash,3);
+  swap(hash,2,hash,3);
+  swapb(hash,0);
+  **/
+  verif[0] = h32[0];
+
+  console.log(`JS verif val: 0x${verif[0].toString(16).padStart(8, '0')}`);
+  console.log(`JS verif val: 0x${verif[0].toString(2).padStart(32, '0')}`);
+	
+	return verif[0];
+}
