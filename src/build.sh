e8862868fede94f38dd4c2531976cda286240e44
Josh Junon <josh@junon.me>
2020-05-24 01:29:43 +0200

    fix arg0 under clang to allow openmp option

--- src/build.sh
+++ src/build.sh
@@ -1,5 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 rm -rf bin
 mkdir bin
-g++ -march=native -fopenmp -Ofast *.cpp -o bin/bebbsum
+if [[ "$(basename "$(readlink "$(which c++)")")" == clang++* ]]; then 
+	clang++ -march=native -fopenmp -Ofast *.cpp -o bin/bebbsum
+else
+	c++ -march=native -fopenmp -Ofast *.cpp -o bin/bebbsum
+fi
