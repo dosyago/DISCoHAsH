49e8d154444bf08afbc5e83f41b88d74ce91dd57
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2023-06-22 02:34:12 +0000

    OK

--- src/test.sh
+++ src/test.sh
@@ -2,15 +2,11 @@
 
 ./build.sh
 
-./bin/bebbsum 32.txt
-./bin/bebbsum 33.txt
-
-
-#CHECK=$(./bin/bebbsum *.txt)
-#VALUE=$(basename *.txt .txt)
-#if [[ $CHECK -eq $VALUE ]]; then
-#  echo "Looks good."
-#else
-#  echo "Check value is wrong. Should be $VALUE"
-#fi
+CHECK=$(./bin/bebbsum 0x*.txt)
+VALUE=$(basename 0x*.txt .txt)
+if [[ $CHECK -eq $VALUE ]]; then
+  echo "Looks good."
+else
+  echo "Check value is wrong. Should be $VALUE"
+fi
 
