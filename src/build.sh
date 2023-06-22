2401ccb7bd94534867bc19ce9555f40d33e6d8ce
Cris <22254235+00000o1@users.noreply.github.com>
2023-06-22 09:19:23 +0800

    Works !

--- src/build.sh
+++ src/build.sh
@@ -4,5 +4,5 @@ rm -rf bin
 mkdir bin
 #-fopenmp unsupported
 #g++ -g -march=native -Ofast *.cpp -o bin/bebbsum
-clang++ -g -std=c++17 -march=native -O3 *.cpp -o bin/bebbsum
+clang++ -fsanitize=address -Wall -g -std=c++17 -march=native -O3 *.cpp -o bin/bebbsum
 
