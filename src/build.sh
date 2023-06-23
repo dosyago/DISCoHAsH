968560cbb0d00e65c8de5e3dede5fb9bc8f34d67
Cris <22254235+00000o1@users.noreply.github.com>
2023-06-24 01:30:41 +0800

    Disco updates

--- src/build.sh
+++ src/build.sh
@@ -3,6 +3,6 @@
 rm -rf bin
 mkdir bin
 #-fopenmp unsupported
-g++ -march=native -Ofast *.cpp -o bin/bebbsum
-#clang++ -fsanitize=address -Wall -gp -std=c++17 -march=native -O3 *.cpp -o bin/bebbsum
+#g++ -march=native -Ofast *.cpp -o bin/bebbsum
+clang++ -fsanitize=address,undefined -Wall -g -std=c++17 -march=native -O3 *.cpp -o bin/bebbsum
 
