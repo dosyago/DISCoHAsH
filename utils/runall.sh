67a31e6bbf8860172754f58818c2bcfdf0bd172a
Cris <22254235+00000o1@users.noreply.github.com>
2023-07-04 08:48:10 +0800

    Update runall

--- utils/runall.sh
+++ utils/runall.sh
@@ -1,4 +1,59 @@
-while read line; do
-  unbuffer ./SMHasher --test=$line $1 > $line.t_$1_$2.results.txt 2>&1 &
-done < names.txt
-watch grep '!!!!' *t_$1_$2.results.txt
+#!/bin/bash
+
+# Check if script and watch are installed
+if ! command -v script &> /dev/null; then
+    echo "script could not be found. Please ensure it is available before running this script."
+    echo "You can also use unbuffer. Modify the script to use it if you have it."
+    exit 1
+fi
+
+if ! command -v watch &> /dev/null; then
+    echo "watch could not be found. Please install it before running this script."
+    exit 1
+fi
+
+# Check if the script is called with the required parameter
+if [ "$#" -ne 1 ]; then
+    echo "Usage: ./script_name <hashName>"
+    exit 1
+fi
+
+tests=(
+    Sanity
+    Speed
+    Hashmap
+    Avalanche
+    Sparse
+    Permutation
+    Window
+    Cyclic
+    TwoBytes
+    Text
+    Zeroes
+    Seed
+    PerlinNoise
+    Diff
+    DiffDist
+    BIC
+    MomentChi2
+    Prng
+    BadSeeds
+)
+
+hashName="$1"
+# Getting current datetime in ISO 8601 format with minute precision
+# Note: The -Iminutes option is not available on macOS's date command by default.
+# We can use an alternative format string:
+datetime=$(date +"%Y-%m-%dT%H:%M")
+
+for test in "${tests[@]}"; do
+    # Use script -q /dev/null to mimic unbuffer
+    script -q /dev/null -c "./SMHasher --test=\"$test\" \"$hashName\"" > "${test}_t_${hashName}_${datetime}.results.txt" 2>&1 &
+done
+
+# Sleep for a short duration to allow the processes to start
+sleep 2
+
+# Watching for output containing '!!!!'
+watch grep '!!!!' *t_${hashName}_${datetime}.results.txt
+
